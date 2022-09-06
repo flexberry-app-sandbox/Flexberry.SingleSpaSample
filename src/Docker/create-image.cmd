ae60a25cff941275dbae1be25c6c8fbe89a6ea66
Bratchikov Igor <bratchikov@neoplatform.ru>
2022-09-06 12:28:09 +0500

    Delete src/Docker directory

--- src/Docker/create-image.cmd
+++
@@ -1,3 +0,0 @@
-docker build --no-cache -f SQL\Dockerfile.PostgreSql -t singlespasample/postgre-sql ../SQL
-
-docker build --no-cache -f Dockerfile -t singlespasample/app ../..
--- src/Docker/start.cmd
+++
@@ -1,1 +0,0 @@
-docker-compose -p singlespasample up -d
